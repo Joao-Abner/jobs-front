2b64ffe4ac205a3be1be7def7457ee2d2d65cefe
Joao Abner <joaoabnerpereira@gmail.com>
2022-05-11 14:28:47 -0300

    Vagas_post

---
+++ src/app/vagas/all-vagas/vagas.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError, empty, map, Observable } from 'rxjs';
+import { Vagas } from './vagas';
+import { VagasService } from './vagas.service';
+
+@Component({
+  selector: 'app-vagas',
+  templateUrl: './vagas.component.html',
+  styleUrls: ['./vagas.component.css']
+})
+export class VagasComponent implements OnInit {
+
+  vagas$!:Observable<Vagas[]>
+
+  constructor(
+    private service:VagasService
+  ) {}
+
+  ngOnInit(): void {
+    this.listar();
+  }
+  listar(){
+    this.vagas$ = this.service.list().pipe(
+      map(result => result.vagas),
+      catchError((error: any) => {
+        console.error(error);
+        return empty();
+      }))
+  }
+
+}
